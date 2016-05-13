cd6f1ab81fb2445467922ddba21138eb26d3466a
Noah Buscher <me@noahbuscher.com>
2016-05-13 18:49:06 -0500

    Customizing files

--- dist/showdown.js
+++ dist/showdown.js
@@ -1258,8 +1258,8 @@ showdown.subParser('blockGamut', function (text, options, globals) {
 
   // we parse blockquotes first so that we can have headings and hrs
   // inside blockquotes
-  text = showdown.subParser('blockQuotes')(text, options, globals);
-  text = showdown.subParser('headers')(text, options, globals);
+  // text = showdown.subParser('blockQuotes')(text, options, globals);
+  // text = showdown.subParser('headers')(text, options, globals);
 
   // Do Horizontal Rules:
   var key = showdown.subParser('hashBlock')('<hr />', options, globals);
@@ -1267,9 +1267,9 @@ showdown.subParser('blockGamut', function (text, options, globals) {
   text = text.replace(/^[ ]{0,2}([ ]?\-[ ]?){3,}[ \t]*$/gm, key);
   text = text.replace(/^[ ]{0,2}([ ]?_[ ]?){3,}[ \t]*$/gm, key);
 
-  text = showdown.subParser('lists')(text, options, globals);
-  text = showdown.subParser('codeBlocks')(text, options, globals);
-  text = showdown.subParser('tables')(text, options, globals);
+  // text = showdown.subParser('lists')(text, options, globals);
+  // text = showdown.subParser('codeBlocks')(text, options, globals);
+  // text = showdown.subParser('tables')(text, options, globals);
 
   // We already ran _HashHTMLBlocks() before, in Markdown(), but that
   // was to escape raw HTML in the original Markdown source. This time,
@@ -1810,14 +1810,14 @@ showdown.subParser('spanGamut', function (text, options, globals) {
   'use strict';
 
   text = globals.converter._dispatch('spanGamut.before', text, options);
-  text = showdown.subParser('codeSpans')(text, options, globals);
+  // text = showdown.subParser('codeSpans')(text, options, globals);
   text = showdown.subParser('escapeSpecialCharsWithinTagAttributes')(text, options, globals);
   text = showdown.subParser('encodeBackslashEscapes')(text, options, globals);
 
   // Process anchor and image tags. Images must come first,
   // because ![foo][f] looks like an anchor.
-  text = showdown.subParser('images')(text, options, globals);
-  text = showdown.subParser('anchors')(text, options, globals);
+  // text = showdown.subParser('images')(text, options, globals);
+  // text = showdown.subParser('anchors')(text, options, globals);
 
   // Make links out of things like `<http://example.com/>`
   // Must come after _DoAnchors(), because you can use < and >
@@ -1825,7 +1825,7 @@ showdown.subParser('spanGamut', function (text, options, globals) {
   text = showdown.subParser('autoLinks')(text, options, globals);
   text = showdown.subParser('encodeAmpsAndAngles')(text, options, globals);
   text = showdown.subParser('italicsAndBold')(text, options, globals);
-  text = showdown.subParser('strikethrough')(text, options, globals);
+  // text = showdown.subParser('strikethrough')(text, options, globals);
 
   // Do hard breaks:
   text = text.replace(/  +\n/g, ' <br />\n');
--- src/subParsers/spanGamut.js
+++ src/subParsers/spanGamut.js
@@ -6,14 +6,14 @@ showdown.subParser('spanGamut', function (text, options, globals) {
   'use strict';
 
   text = globals.converter._dispatch('spanGamut.before', text, options);
-  text = showdown.subParser('codeSpans')(text, options, globals);
+  // text = showdown.subParser('codeSpans')(text, options, globals);
   text = showdown.subParser('escapeSpecialCharsWithinTagAttributes')(text, options, globals);
   text = showdown.subParser('encodeBackslashEscapes')(text, options, globals);
 
   // Process anchor and image tags. Images must come first,
   // because ![foo][f] looks like an anchor.
-  text = showdown.subParser('images')(text, options, globals);
-  text = showdown.subParser('anchors')(text, options, globals);
+  // text = showdown.subParser('images')(text, options, globals);
+  // text = showdown.subParser('anchors')(text, options, globals);
 
   // Make links out of things like `<http://example.com/>`
   // Must come after _DoAnchors(), because you can use < and >
@@ -21,7 +21,7 @@ showdown.subParser('spanGamut', function (text, options, globals) {
   text = showdown.subParser('autoLinks')(text, options, globals);
   text = showdown.subParser('encodeAmpsAndAngles')(text, options, globals);
   text = showdown.subParser('italicsAndBold')(text, options, globals);
-  text = showdown.subParser('strikethrough')(text, options, globals);
+  // text = showdown.subParser('strikethrough')(text, options, globals);
 
   // Do hard breaks:
   text = text.replace(/  +\n/g, ' <br />\n');
